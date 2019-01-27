0e58874bc04b2111177c4bd6d5bd6cda58bfdc68
Yuma Sumi <rbly.776324.yuma66krayt@gmail.com>
2019-01-27 18:10:57 +0900

    fixed a bug in modify

--- services/db/balletCompanies/modify.js
+++ services/db/balletCompanies/modify.js
@@ -1,24 +1,25 @@
 module.exports = (knex, BalletCompany) => {
   return (params) => {
-    return (
-      knex("balletCompanies")
-        .where("id", params.id)
-        //   .update(params.toBe)
-        .then((companies) => {
-          companies.forEach((company) => {
-            for (const key in params.toBe) {
-              company[key] = params.toBe[key];
-            }
-          });
-          return companies;
-        })
-        .then((companies) => {
-          return companies.map((company) => new BalletCompany(company));
-        })
-        .catch((err) => {
-          // throw unknown errors
-          throw err;
-        })
-    );
+    return knex("balletCompanies")
+      .where("id", params.id)
+      .update(params.toBe)
+      .then(() => {
+        return knex("balletCompanies").select();
+      })
+      .then((companies) => {
+        companies.forEach((company) => {
+          for (const key in params.toBe) {
+            company[key] = params.toBe[key];
+          }
+        });
+        return companies;
+      })
+      .then((companies) => {
+        return companies.map((company) => new BalletCompany(company));
+      })
+      .catch((err) => {
+        // throw unknown errors
+        throw err;
+      });
   };
 };
